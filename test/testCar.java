c03297ea15e0d9151e749bc821ecf08556799e35
linahva <lina@johansson.bz>
2024-01-18 11:27:37 +0100

    börjat med test

---
+++ test/testCar.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testCar {
+    @Test
+    public void testStartEnginge() {
+        Car car = new Volvo240();
+        car.startEngine();
+
+        assertEquals()
+    }
+}
